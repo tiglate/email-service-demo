cd9c30714f9b4749874d5961345c7c4841e257e7
tiglate <128345445+tiglate@users.noreply.github.com>
2025-10-12 00:34:28 -0300

    Add Docker Compose setup, environment variables for configuration, and initialization scripts for database services

---
+++ init-scripts/auctoritas.sql
@@ -0,0 +1,3 @@
+CREATE USER usr_auctoritas_dev WITH PASSWORD '12345';
+CREATE DATABASE auctoritas;
+ALTER DATABASE auctoritas OWNER TO usr_auctoritas_dev;
---
+++ init-scripts/tabellarius.sql
@@ -0,0 +1,3 @@
+CREATE USER usr_tabellarius_dev WITH PASSWORD '12345';
+CREATE DATABASE tabellarius;
+ALTER DATABASE tabellarius OWNER TO usr_tabellarius_dev;
